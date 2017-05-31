809fb4e8171cdfa3e3d3b553d7b2ad89c1e135a9
polrodoreda <pol_rodoreda@hotmail.com>
2017-05-31 12:54:27 +0200

    Add help page and some images

--- travelapp/urls.py
+++ travelapp/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 from . import views
 
 
@@ -6,4 +7,5 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^travel/form/$', views.travel_form, name='travel_form'),
     url(r'^travel/info/$', views.travel_info, name='travel_info'),
+    url(r'^help/$', TemplateView.as_view(template_name='help.html'), name='help'),
 ]
